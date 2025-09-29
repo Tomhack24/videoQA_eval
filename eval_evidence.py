792c119bc4832d86005548d4516a775680c06cb8
tomoki_from_abci <yoshioka.0315527@gmail.com>
2025-09-29 19:08:30 +0900

    update

--- eval_evidence.py
+++ eval_evidence.py
@@ -3,11 +3,11 @@ import pandas as pd
 from utils import culcurate_recall_precision
 
 QA_DATASET_GEMINI_PATH = "GROUND_TRUTH/QA_DATASET_GEMINI.jsonl"
-PREDICT_QWEN_VL_7B_PATH = "PREDICT/QWEN_VL_7B.jsonl"
+PREDICT_QWEN_VL_PATH = "PREDICT/QA_DATASET_INFERENCE_32B.jsonl"
 
 def main():
     df_gt = pd.read_json(QA_DATASET_GEMINI_PATH, lines=True)
-    df_pr = pd.read_json(PREDICT_QWEN_VL_7B_PATH, lines=True)
+    df_pr = pd.read_json(PREDICT_QWEN_VL_PATH, lines=True)
     df_pr = df_pr.rename(columns={"Answer": "Predicted Answer", "Evidence": "Predicted Evidence"})
 
     merged_df = pd.merge(df_gt, df_pr, on="QA_number", how="inner")
@@ -20,6 +20,10 @@ def main():
     print(f"Recall: {recall:.4f}")
     print(f"Precision: {precision:.4f}")
     print(f"Exact Match: {exact_match}")
+    PREDICT_QWEN_VL_CSV = "PREDICT/QA_DATASET_INFERENCE.csv"
+    PREDICT_ID = PREDICT_QWEN_VL_PATH.split("/")[1].replace("QA_DATASET_INFERENCE_", "").replace(".jsonl", "")
+    with open(PREDICT_QWEN_VL_CSV, "a") as f:
+        f.write(f"{PREDICT_ID},"f"{recall:.4f},{precision:.4f},{exact_match}\n")
 
 if __name__ == "__main__":
     main()
